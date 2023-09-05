483e4e160f6145d9d6e78c6b7784ebfff99fc97c
oahmed105 <oahmed10598@gmail.com>
2023-09-05 14:45:46 -0400

    react boiiiiii

--- newscraft/src/main/webapp/app/shared/layout/header/header-components.tsx
+++ newscraft/src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import { NavItem, NavLink, NavbarBrand } from 'reactstrap';
 import { NavLink as Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon as ExtraIcons } from '@fortawesome/fontawesome-common-types';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
@@ -25,16 +23,6 @@ export const Home = () => (
     <NavLink tag={Link} to="/" className="d-flex align-items-center">
       <FontAwesomeIcon icon="home" />
       <span>Home</span>
-    </NavLink>
-  </NavItem>
-);
-
-export const Articles = () => (
-  <NavItem>
-    <NavLink tag={Link} to="/articlelist" className="d-flex align-items-center">
-{/*       <FontAwesomeIcon icon="home" /> */}
-      <FontAwesomeIcon icon={faNewspaper} />
-      <span>Articles</span>
     </NavLink>
   </NavItem>
 );
