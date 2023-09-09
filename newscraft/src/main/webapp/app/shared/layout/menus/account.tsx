fbe1625e39b1913a84abb840de173085db228a34
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-09-08 20:13:26 -0400

    bookmarks baseline created

--- newscraft/src/main/webapp/app/shared/layout/menus/account.tsx
+++ newscraft/src/main/webapp/app/shared/layout/menus/account.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import MenuItem from 'app/shared/layout/menus/menu-item';
+import { NavLink as Link } from 'react-router-dom';
+import { DropdownItem } from 'reactstrap';
 
+import MenuItem from 'app/shared/layout/menus/menu-item';
 import { NavDropdown } from './menu-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const accountMenuItemsAuthenticated = () => (
   <>
+    <DropdownItem tag={Link} to="/account/bookmarks" data-cy="bookmarks">
+      <FontAwesomeIcon icon={faBookmark} fixedWidth /> Bookmarks
+    </DropdownItem>
+{/*     <NavDropdown.Divider /> */}
     <MenuItem icon="wrench" to="/account/settings" data-cy="settings">
       Settings
     </MenuItem>
