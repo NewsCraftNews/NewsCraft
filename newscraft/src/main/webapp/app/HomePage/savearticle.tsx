1ba9c09b33ab301acd8acc9b84a9658f8c668fb2
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-09-11 17:05:30 -0400

    saving

--- newscraft/src/main/webapp/app/HomePage/savearticle.tsx
+++ newscraft/src/main/webapp/app/HomePage/savearticle.tsx
@@ -31,7 +31,7 @@ export const BookmarkButton = (props: IArticleProps) => {
   }, [login, id]);
 
   useEffect(() => {
-    var newToggleVal: boolean = loading ? false : bookMarkEntity;
+    var newToggleVal: boolean = !loading && bookMarkEntity ? true : false;
     setToggle(newToggleVal);
   });
 
