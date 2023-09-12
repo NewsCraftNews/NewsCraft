886ba3d35bf0cf01953f6a8adb0401733734bdb4
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-09-11 20:39:07 -0400

    the button toggles back and forth better than before!

--- newscraft/src/main/webapp/app/HomePage/savearticle.tsx
+++ newscraft/src/main/webapp/app/HomePage/savearticle.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Button, ButtonToggle } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as fasBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as farBookmark } from '@fortawesome/free-regular-svg-icons';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 
@@ -30,11 +31,6 @@ export const BookmarkButton = (props: IArticleProps) => {
     dispatch(getUserSpecificEntity({login, id}));
   }, [login, id]);
 
-  useEffect(() => {
-    let newToggleVal: boolean = !!(!loading && bookMarkEntity);
-    setToggle(newToggleVal);
-  });
-
   const handleClick = () => {
     if(bookMarkEntity.id === undefined){
       // if the bookmark doesn't exist then save
@@ -46,12 +42,13 @@ export const BookmarkButton = (props: IArticleProps) => {
     else {
       dispatch(deleteEntity(bookMarkEntity.id));
     }
+    setToggle(!toggle);
   };
 
 // it looks ugly now BUT IT WORKS!
   return ( isAuthenticated ?
-    <Button className="me-2" color="info" onClick={handleClick} active={toggle}>
-      <FontAwesomeIcon icon={faBookmark} />
+    <Button className="me-2" color="info" onClick={handleClick} toggle={toggle}>
+      {toggle ? <FontAwesomeIcon icon={fasBookmark} /> : <FontAwesomeIcon icon={farBookmark} />}
     </Button>
     :
     <span>&nbsp;</span>
