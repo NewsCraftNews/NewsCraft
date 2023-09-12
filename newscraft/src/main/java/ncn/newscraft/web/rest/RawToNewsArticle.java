c1c287cbca2ef5c6a01de662e69f8c61944b8e31
santos <santoslopez890@gmail.com>
2023-09-12 16:27:34 -0400

    fixed some stuffff Yeah

--- newscraft/src/main/java/ncn/newscraft/web/rest/RawToNewsArticle.java
+++ newscraft/src/main/java/ncn/newscraft/web/rest/RawToNewsArticle.java
@@ -5,10 +5,13 @@ import ncn.newscraft.domain.NewsArticle;
 import ncn.newscraft.domain.NewsArticleRaw;
 import ncn.newscraft.service.ExternalApis;
 import ncn.newscraft.service.mapper.ExternalApiTransactionalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +20,7 @@ import java.util.List;
 @RequestMapping("/api/news")
 
 public class RawToNewsArticle {
+    private final Logger log = LoggerFactory.getLogger(RawToNewsArticle.class);
 
     @Autowired
     private ExternalApiTransactionalService newsArticleService;
@@ -24,11 +28,15 @@ public class RawToNewsArticle {
     ExternalApis externalApis=new ExternalApis();
 
     @GetMapping("/fetch-and-save")
-    public ResponseEntity<String> fetchAndSaveNews() throws JsonProcessingException {
+    public ResponseEntity<String> fetchAndSaveNews(
+        @RequestParam String category,
+        @RequestParam String size
+    ) throws JsonProcessingException {
         // Fetch news articles from the external API
-        List<NewsArticleRaw> articles = externalApis.fetchNewsArticles(); // Fetch and map raw articles here
-            // Save the articles to the database
-            newsArticleService.saveToDataBase(articles);
+        log.debug("request params are {} {}", category, size);
+        List<NewsArticleRaw> articles = externalApis.fetchNewsArticles(category, size); // Fetch and map raw articles here
+        // Save the articles to the database
+        newsArticleService.saveToDataBase(articles);
         return ResponseEntity.ok("News articles saved successfully.");
     }
 
